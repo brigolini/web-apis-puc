43012f2dfa2c4ee994f2c2d525db0bafa86e1ce9
Marcelo Brigolini <mbrigolinisilva@trueblue.com>
2023-06-13 20:56:45 -0300

    simple rest

--- rest/index.js
+++ rest/index.js
@@ -1,10 +1,50 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
-})
+const rootEndpoint = "/api";
 
+const veiculosEndpoint = `${rootEndpoint}/veiculos`;
+
+const veiculos = [
+    {id:0, placa:"ABC-1234", modelo:"Fusca", marca:"Volkswagen", ano:1970, cor:"Azul"},
+    {id:1, placa:"DEF-5678", modelo:"Gol", marca:"Volkswagen", ano:2000, cor:"Preto"},
+    {id:2, placa:"GHI-9012", modelo:"Uno", marca:"Fiat", ano:2005, cor:"Branco"},
+];
+app.get(`${veiculosEndpoint}`, function (req, res) {
+res.send(veiculos);
+});
+
+app.get(`${veiculosEndpoint}/:id`, function (req, res) {
+    const id = req.params.id;
+    const veiculo = veiculos[id];
+    res.status(200).send(veiculo);
+});
+
+app.post(`${veiculosEndpoint}`, function (req, res) {
+    const veiculo = req.body;
+    veiculos.push(veiculo);
+    res.status(201).send(veiculo);
+});
+
+app.put(`${veiculosEndpoint}/:id`, function (req, res) {
+    const id = req.params.id;
+    const veiculo = req.body;
+    veiculos[id] = veiculo;
+    res.status(200).send(veiculo);
+});
+
+app.patch(`${veiculosEndpoint}/:id`, function (req, res) {
+    const id = req.params.id;
+    const veiculo = req.body;
+    veiculos[id] = veiculo;
+    res.status(200).send(veiculo);
+});
+
+app.delete(`${veiculosEndpoint}/:id`, function (req, res) {
+    const id = req.params.id;
+    veiculos.splice(id, 1);
+    res.status(204).send("OK");
+});
 var server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
